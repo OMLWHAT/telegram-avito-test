0024f0a82498fe46f4482b2e3764bac3ed5deee0
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-10-31 00:16:48 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -23,7 +23,7 @@ class Bot:
         main__div = soup.find_all('div', {'class': 'ON2y7'})
         for data in main__div:
             try:
-                a = data.find('a', {'class': 'xTVII'})['href']
+                a = data.find('a', {'class': 'EfomW'})['itemprop']
                 links = {
                     'href': f'https://m.avito.ru{a}'
                 }
