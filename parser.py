779811ff00b8f1c6fc202fbdca3c107794413ca1
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-11-02 19:38:01 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,44 +1,11 @@
 import requests
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
+url = "https://www.avito.ru/naberezhnye_chelny/nedvizhimost"
+request = requests.get(url)
+bs = BeautifulSoup(request.text, "html.parser")
 
-class Bot:
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
-    }
+all_links = bs.find_all("a", class_="link-link-MbQDP")
 
-    def __init__(self):
-        self.get_html()
-
-    def get_html(self):
-        url = "https://avito.ru/naberezhnye_chelny/nedvizhimost"
-        with requests.Session() as session:
-            response = session.get(url, headers=self.headers)
-        return response.text
-
-    def parse_html(self, html):
-        urls = []
-        soup = bs(html, 'lxml')
-        main__div = soup.find_all('div', {'class': 'link-link-MbQDP'})
-        for data in main__div :
-            try:
-                a = data['href']
-                links = {
-                    'href': f'https://m.avito.ru{a}'
-                }
-                urls.append(links)
-            except:
-                continue
-        return urls
-
-
-def main():
-    bot = Bot()
-    html = bot.get_html()
-    result = bot.parse_html(html)
-    print(result)
-
-
-if __name__ == '__main__':
-    main()
+for links in all_links:
+    print(links["href"])
