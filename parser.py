81fa67143ce694241e40149031eb8bbeb51edded
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-11-02 19:39:07 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ url = "https://www.avito.ru/naberezhnye_chelny/nedvizhimost"
 request = requests.get(url)
 bs = BeautifulSoup(request.text, "html.parser")
 
-all_links = bs.find_all("a", class_="link-link-MbQDP")
+all_links = bs.find_all("a", class_="link-link-MbQDP")["href"]
 
 for links in all_links:
-    print(links["href"])
+    print(links)
