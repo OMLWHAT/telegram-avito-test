f7669b039961a00ecd4d523220e5a8706141e98c
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-11-02 19:54:50 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-import time
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,8 +5,7 @@ url = "https://www.avito.ru/naberezhnye_chelny/nedvizhimost"
 request = requests.get(url)
 bs = BeautifulSoup(request.text, "html.parser")
 
-all_links = bs.find_all("a", {"itemprop": "url"})
+all_links = bs.find_all("a", {"data-marker": "title"})
 
 for links in all_links:
     print("https://www.avito.ru" + links["href"])
-    time.sleep(1)
