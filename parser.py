90bd54daf312c99993e51d3b696bb782ca95b2bc
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-11-02 22:35:51 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,13 +1,51 @@
+import urllib.parse
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.avito.ru/tatarstan/bytovaya_elektronika?f=ASgCAgECAUXGmgwVeyJmcm9tIjoxLCJ0byI6MTUwMDB9&q=Видеокарты"
-request = requests.get(url)
-bs = BeautifulSoup(request.text, "html.parser")
+urls = []
 
-all_urls = []
-all_links = bs.find_all("a", {"itemprop": "url"})
+class AvitoParser:
+    def get_page():
+        url = "https://www.avito.ru/tatarstan/bytovaya_elektronika?f=ASgCAgECAUXGmgwVeyJmcm9tIjoxLCJ0byI6MTUwMDB9&q=Видеокарты"
+        request = requests.get(url)
+        bs = BeautifulSoup(request.text, "html.parser")
+    
+    def get_pagination_limit(self):
+        text = self.get_page()
+        bs = BeautifulSoup(text, "lxml")
 
-for link in all_links:
-    all_urls.append("https://www.avito.ru" + link["href"])
-print(all_urls)
+        container = bs.select("a.pagination-page")
+        last_button = container[-1]
+        href = last_button.get("href")
+        if not href:
+            return 1
+        
+        r = urllib.parse.urlparse(href)
+        params = urllib.parse.parse_qs(r.query)
+        
+        return int(params["p"][0])
+
+    def get_blocks(self, page: int = None):
+        text = self.get_page(page=page)
+        bs = BeautifulSoup(text, "lxml")
+
+        container = bs.select("div.item.item_table.clearfix.js-catalog-item-enum.item-with-contact.js-item-extended")
+        
+        for item in container:
+            block = self.parse_block(item=item)
+            urls.append(block)
+
+    def parse_all(self):
+        limit = self.get_pagination_limit()
+
+        for i in range(1, limit + 1):
+            self.get_blocks(page=i)
+            
+def main():
+    p = AvitoParser()
+    p.parse_all()
+    print(urls)
+
+if __name__ == "__main__":
+    main()
