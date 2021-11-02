787f21b83b227643bb1ded9d40d7eb1c12521c03
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-11-02 22:54:09 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -26,8 +26,8 @@ class AvitoParser:
         
         return int(params["p"][0])
 
-    def get_blocks(self, page: int = None):
-        text = self.get_page(page=page)
+    def get_blocks(self, page):
+        text = self.get_page(page)
         bs = BeautifulSoup(text, "lxml")
 
         container = bs.select("div.item.item_table.clearfix.js-catalog-item-enum.item-with-contact.js-item-extended")
