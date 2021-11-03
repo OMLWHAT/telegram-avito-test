739aef1ca62d374790b6a127477d66a544417945
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-11-03 13:26:37 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,33 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 
+total_urls = []
+
 class Bot():
     def get_page(self, page):
         url = f"https://www.avito.ru/tatarstan/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw?f=ASgBAgECAkTGB~pm7gmmZwFFxpoMFXsiZnJvbSI6MSwidG8iOjE1MDAwfQ&p={page}"
         request = requests.get(url)
+        
         return request.text
+    
+    def get_ads(self, pages):
+        text = self.get_page(pages)
+        bs = BeautifulSoup(text, "html.parser")
+        
+        urls = []
+        ads = bs.find_all("a", {"itemprop": "url"})
+        
+        for ad in ads:
+            urls.append("https://www.avito.ru" + ad["href"])
+            
+        return urls
+        
+        
     def count_pages(self):
         text = self.get_page(1)
         bs = BeautifulSoup(text, "html.parser")
         
         container = bs.select('a.pagination-page')
         last_button = container[-1]
-        href = last_button.get('href')
-        url = href.rsplit("=", 1)[-1]
-        print(url)
+        url = last_button.get('href')
+        pages = href.rsplit("=", 1)[-1]
+        
+        return pages
         
     def parse(self):
         pages = self.count_pages()
+        
+        for block in range(1, pages + 1):
+            blocks = self.get_ads(block)
+            total_urls.append(blocks)
+            
         
    
 def main():
     parser = Bot()
     parser.count_pages()
     
-main()
-
-# all_urls = []
-#all_links = bs.find_all("a", {"itemprop": "url"})
-
-#for link in all_links:
-    #all_urls.append("https://www.avito.ru" + link["href"])
+if __name__ == '__main__':
+    main()
