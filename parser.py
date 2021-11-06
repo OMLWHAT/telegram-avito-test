a4200fa51c9b28b3d1543336c7baa8d638d2d49e
Quynaus <52112535+OMLWHAT@users.noreply.github.com>
2021-11-06 22:31:27 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,17 +3,16 @@ import requests
 from itertools import groupby
 from bs4 import BeautifulSoup
 
-avito_url = "https://www.avito.ru/tatarstan/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw?f=ASgBAgECAkTGB~pm7gmmZwFFxpoMFXsiZnJvbSI6MSwidG8iOjE1MDAwfQ&p="
-
 urls = []
 
 class Bot():
-    def get_page(self, url, page: None):
-        url = ""
-        if page == None:
-            url = f"{url}"
-        else:
-            url = f"{url}{page}"
+    def get_ads_page(self, url):
+        request = requests.get(url)
+        
+        return request.text
+    
+    def get_page(self, page):
+        url = f"https://www.avito.ru/tatarstan/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw?f=ASgBAgECAkTGB~pm7gmmZwFFxpoMFXsiZnJvbSI6MSwidG8iOjE1MDAwfQ&p={page}"
         request = requests.get(url)
         
         return request.text
@@ -27,7 +26,7 @@ class Bot():
            
             for ad in ads:
                 url = "https://www.avito.ru" + ad["href"]
-                ad_text = self.get_page(url, page=page)
+                ad_text = self.get_ads_page(url)
                 ad_bs = BeautifulSoup(ad_text, "html.parser")
                 
                 ad_page = ad_bs.find("div", {"class": "title-info-metadata-item-redesign"})
@@ -39,7 +38,7 @@ class Bot():
         
         
     def count_pages(self):
-        text = self.get_page(avito_url, page=1)
+        text = self.get_page(avito_url)
         bs = BeautifulSoup(text, "html.parser")
         
         buttons = bs.select("a.pagination-page")
